bfa6cdbb165b49cf5615fd470f252e5210ab1212
Viltco <58694110+Viltco@users.noreply.github.com>
2022-01-19 15:21:30 +0500

    change_sale_date

---
+++ change_sale_date/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Change Sale Date",
+    'summary': """Adds a date field in sale order to be used a order date""",
+    'description': """Adds a date field in sale order to be used a order date""",
+    'author': "SolutionFounder",
+    'website': "http://www.solutionfounder.com",
+    'category': 'sale',
+    'version': '14.0.0.2',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale.xml',
+    ],
+}
